c42245d123f9c6b5101af9497ce515a4871110d4
shitalc <shitalchatap1234@gmail.com>
2022-12-27 12:11:59 +0530

    Student Program

---
+++ SchoolManagement/Program.cs
@@ -0,0 +1,75 @@
+﻿using SchoolManagement;
+
+namespace Maveric.Runner
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*********************STUDENT DASHBORD*******************");
+            Student.schoolName = "Global school";
+            Student.schoolAddress = "chennai";
+
+            Student std1 = new Student();
+            Student std2 = new Student();
+            Student std3 = new Student();
+
+            std1.studentId = 1001;
+            std1.studentName = "jack";
+            std1.studentMailid = "jack@gmail.com";
+            std1.studentPercentage = 45.2;
+            std1.PERCENTAGEGRADE = 'C';
+            Student.schoolName = "Global school";
+            Student.schoolAddress = "chennai";
+
+
+            Console.WriteLine(std1.studentId);
+            Console.WriteLine(std1.studentName);
+            Console.WriteLine(std1.studentMailid);
+            Console.WriteLine(std1.studentPercentage);
+            Console.WriteLine(std1.PERCENTAGEGRADE);
+
+
+            std2.studentId = 1002;
+            std2.studentName = "peter";
+            std2.studentMailid = "peter@gmail.com";
+            std2.studentPercentage = 85.2;
+            std2.PERCENTAGEGRADE = 'B';
+            Student.schoolName = "Global school";
+            Student.schoolAddress = "chennai";
+
+
+
+            Console.WriteLine(std2.studentId);
+            Console.WriteLine(std2.studentName);
+            Console.WriteLine(std2.studentMailid);
+            Console.WriteLine(std2.studentPercentage);
+            Console.WriteLine(std2.PERCENTAGEGRADE);
+
+            std3.studentId = 1003;
+            std3.studentName = "mark";
+            std3.studentMailid = "mark@gmail.com";
+            std3.studentPercentage = 56.5;
+            std3.PERCENTAGEGRADE = 'C';
+            Student.schoolName = "Global school";
+            Student.schoolAddress = "chennai";
+
+            Console.WriteLine(std3.studentId);
+            Console.WriteLine(std3.studentName);
+            Console.WriteLine(std3.studentMailid);
+            Console.WriteLine(std3.studentPercentage);
+            Console.WriteLine(std3.PERCENTAGEGRADE);
+
+
+            std1.PrintStudentDetails();
+            std2.PrintStudentDetails();
+            std3.PrintStudentDetails();
+            //  emp3.PrintEmployeeDetail();
+
+            std1.DisplayCertification();
+            std2.DisplayCertification();
+            std3.DisplayCertification();
+
+        }
+    }
+}
---
+++ SchoolManagement/Student.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.ConstrainedExecution;
+
+namespace SchoolManagement
+{
+    public class Student
+    {
+        public int studentId;//non static variable or instance varible 
+        public string studentName;
+        public string studentMailid;
+        public double studentPercentage;
+        public char PERCENTAGEGRADE;
+        public static string schoolName;//static variable or class varible
+        public static string schoolAddress;
+        public void PrintStudentDetails()
+        {
+            Console.WriteLine("Student Id: " + studentId);
+            Console.WriteLine("Student name: " + studentName);
+            Console.WriteLine("Student Mail Id:" + studentMailid);
+            Console.WriteLine("Student Percentage: " + studentPercentage);
+            Console.WriteLine("PERCENTAGE  GRADE: "+ PERCENTAGEGRADE);
+            Console.WriteLine("School Name: " + Student.schoolName);
+            Console.WriteLine("School Address: " + Student.schoolAddress);
+
+            Console.WriteLine("----------------------------------------------------");
+
+        }
+        public void DisplayCertification()
+        {
+            Console.WriteLine("Student percentage: " + studentPercentage);
+            if (studentPercentage > 90)
+            {
+                PERCENTAGEGRADE = 'A';
+                Console.WriteLine("You have successfully passed with grade A");
+            }
+            else
+            if (studentPercentage >= 80 && studentPercentage <=89)
+            {
+                PERCENTAGEGRADE = 'B';
+                Console.WriteLine("You have successfully passed with grade B");
+
+            }
+            else
+                if (studentPercentage >= 60 && studentPercentage <= 79)
+            {
+                PERCENTAGEGRADE = 'C';
+                Console.WriteLine("You have successfully passed with grade D");
+            }
+            else
+                if(studentPercentage < 60)
+            {
+                PERCENTAGEGRADE = 'D';
+                Console.WriteLine("You have successfully passed with grade C");
+            }
+            else
+            {
+                PERCENTAGEGRADE = 'F';
+            }
+            
+
+
+
+            }
+
+    }
+}
