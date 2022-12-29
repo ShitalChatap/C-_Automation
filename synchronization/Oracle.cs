1df3c885f27429a6eefc69280459e9ee1e3497ba
shitalc <shitalchatap1234@gmail.com>
2022-12-29 14:33:04 +0530

    assignment

---
+++ synchronization/Oracle.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace synchronization
+{
+    public class Oracle
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            driver.Url = "https://www.oracle.com/in/database/";
+            driver.FindElement(By.XPath("//span[text()='View Accounts']")).Click();
+            driver.FindElement(By.XPath("//a[text()='Sign-In']")).Click();
+            Console.WriteLine("The Title of the page: " + driver.Title);
+            Console.WriteLine("The url of the current page: " + driver.Url);
+
+          
+            driver.FindElement(By.XPath("//input[@name='ssousername']")).SendKeys("john");
+            driver.FindElement(By.XPath("//input[@name='password']")).SendKeys("john123");
+            driver.FindElement(By.XPath("//input[@class='sign-in-button']")).Click();
+
+
+
+
+            IWebElement text2 = driver.FindElement(By.XPath("(//div[@class='cb41error'])[2]"));
+            Console.WriteLine(" Error message after invalid username and password:  " + text2.Text);
+
+
+
+
+        }
+    }
+}
