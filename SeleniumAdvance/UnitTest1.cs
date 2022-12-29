71a9e82c0595decddd4ec54bc5e805af3cd71d95
shitalc <shitalchatap1234@gmail.com>
2022-12-29 17:01:26 +0530

    updated

---
+++ SeleniumAdvance/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SeleniumAdvance
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
--- synchronization/Program.cs
+++ synchronization/Program.cs
@@ -1,5 +1,8 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System.Runtime.InteropServices;
 
 namespace Maveric.Synchonization
 {
@@ -17,7 +20,37 @@ namespace Maveric.Synchonization
             driver.Url = "  https://www.goto.com/meeting/";
             driver.FindElement(By.XPath("//button[text()='Accept Recommended Settings']")).Click();
 
-            driver.FindElement(By.XPath("//a[text()='Try Free']")).Click();
+
+
+            Actions actions = new Actions(driver);
+            try
+            {
+                actions.ScrollToElement(driver.FindElement(By.LinkText("Try Free"))).Perform();
+            }
+            catch
+            {
+
+            }
+            driver.FindElement(By.LinkText("Try Free")).Click();
+            driver.FindElement(By.XPath("//input[@name='FirstName']")).SendKeys("John");
+            driver.FindElement(By.XPath("//input[@name='LastName']")).SendKeys("wick");
+            driver.FindElement(By.XPath("//input[@name='Email']")).SendKeys("john@gmail.com");
+            SelectElement Companysize = new SelectElement(driver.FindElement(By.XPath("//select[@id='CompanySize']")));
+            Companysize.SelectByText("10 - 99");
+            driver.FindElement(By.Id("//button[text()='Start My Trial']")).Click();
+            driver.FindElement(By.Id("contact-number")).SendKeys("1A");
+            driver.FindElement(By.XPath("//button[text()='Start My Trial']")).Click();
+            string err = driver.FindElement(By.XPath("//div[@class='inputField__requirements']")).Text;
+            Console.WriteLine(err);
+
+        }
         }
     }
-}
+
+    
+
+
+
+
+        
+    
